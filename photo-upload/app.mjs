1cb9b918876d62088066c6a06b236f44b76281b3
Alejandro Sierra <alexsierra1598@gmail.com>
2023-12-11 21:25:12 -0500

    Add lambda authorizer

--- photo-upload/app.mjs
+++ photo-upload/app.mjs
@@ -1,3 +1,5 @@
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+
 /**
  *
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
@@ -14,9 +16,31 @@
 export const lambdaHandler = async (event, context) => {
     try {
         const userId = event.queryStringParameters.userId
+        const headers = {
+            'Content-Type': 'application/json',
+            "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+            "Access-Control-Allow-Origin": "*", // this will be the site url
+            "Access-Control-Allow-Methods": "*",
+            "X-Requested-With": "*"
+        };
+        const s3Client = new S3Client({})
+        const bucketName = 'photo-app-api-photosbucket-xc1en19z1ai';
+        const key = 'file.txt'; 
+        const data = `This is the userId: ${userId}`;
+
+        const command = new PutObjectCommand({
+            Bucket: bucketName, 
+            Key: key,
+            Body: data
+          });
+          
+        const response = await s3Client.send(command);
+        
+        console.log(response)
 
         return {
             'statusCode': 200,
+            headers,
             'body': JSON.stringify({
                 message: `Photo succesfully uploaded for ${userId}`,
             })
